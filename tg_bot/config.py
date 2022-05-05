40c895a805d093d99b596c83086dfefdc7d60e3a
KuranovaPolina <99879272+KuranovaPolina@users.noreply.github.com>
2022-05-05 16:24:15 +0300

    Database DELETED!!!

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -4,14 +4,6 @@ from environs import Env
 
 
 @dataclass
-class DbConfig:
-    host: str
-    password: str
-    user: str
-    database: str
-
-
-@dataclass
 class TgBot:
     token: str
     admin_ids: list[int]
@@ -19,15 +11,8 @@ class TgBot:
 
 
 @dataclass
-class Miscellaneous:
-    other_params: str = None
-
-
-@dataclass
 class Config:
     tg_bot: TgBot
-    db: DbConfig
-    misc: Miscellaneous
 
 
 def load_config(path: str = None):
@@ -39,12 +24,5 @@ def load_config(path: str = None):
             token=env.str("BOT_TOKEN"),
             admin_ids=list(map(int, env.list("ADMINS"))),
             use_redis=env.bool("USE_REDIS"),
-        ),
-        db=DbConfig(
-            host=env.str('DB_HOST'),
-            password=env.str('DB_PASS'),
-            user=env.str('DB_USER'),
-            database=env.str('DB_NAME')
-        ),
-        misc=Miscellaneous()
+        )
     )
