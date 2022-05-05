d061c76e55c896abd041752e52dc150d40fb4b4f
KuranovaPolina <99879272+KuranovaPolina@users.noreply.github.com>
2022-05-05 11:31:47 +0300

    Unnecessary code from bot.py are deleted

--- bot.py
+++ bot.py
@@ -6,30 +6,16 @@ from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from tg_bot.config import load_config
-# from tg_bot.filters1.admin import AdminFilter
-# from tg_bot.handlers.admin import register_admin
 from tg_bot.handlers.timetable import register_timetable, \
     register_pr_day, \
     register_next_day, \
     register_change_week
-# from tg_bot.middlewares1.db import DbMiddleware
 
 
 logger = logging.getLogger(__name__)
 
 
-def register_all_middlewares(dp):
-    # dp.setup_middleware(DbMiddleware())
-    pass
-
-
-def register_all_filters(dp):
-    # dp.filters_factory.bind(AdminFilter)
-    pass
-
-
 def register_all_handlers(dp):
-    # register_admin(dp)
     register_timetable(dp)
     register_pr_day(dp)
     register_next_day(dp)
@@ -48,8 +34,6 @@ async def main():
     dp = Dispatcher(bot, storage=storage)
     bot['config'] = config
 
-    register_all_middlewares(dp)
-    register_all_filters(dp)
     register_all_handlers(dp)
 
     try:
