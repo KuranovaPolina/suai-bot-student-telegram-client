ff4280010cada26487066e6f5a0c9545696088b3
KuranovaPolina <99879272+KuranovaPolina@users.noreply.github.com>
2022-05-05 17:21:09 +0300

    storage deleted from bot.py

--- bot.py
+++ bot.py
@@ -24,8 +24,7 @@ async def main():
     config = load_config(".env")
 
     bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
-    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
-    dp = Dispatcher(bot, storage=storage)
+    dp = Dispatcher(bot)
     bot['config'] = config
 
     register_all_handlers(dp)
