9fadf2e781a99e3fc51bf8741f2bc70d803eb06e
callmemars1 <callmemars111@gmail.com>
2022-05-16 21:02:30 +0300

    added simple client to get and save message states

--- tg_bot/handlers/timetable.py
+++ tg_bot/handlers/timetable.py
@@ -1,24 +1,40 @@
 import logging
+import time
 
 from aiogram import Dispatcher
 from aiogram.types import Message, CallbackQuery
+
+import TimetableMessageStateDto
+from TimetableMessageStatesMongoClient import TimetableMessageStatesMongoClient
 from tg_bot.keyboards.inline import days_scroll
 from tg_bot.text_format.timetable_text import format_timetable_text
 from tg_bot.users import users, User, Day
+
+# Пробросить через DI
+client = TimetableMessageStatesMongoClient('mongodb://localhost:27017')
 
 
 async def display_timetable(message: Message):
     user = message.from_user.id
 
-    if user not in users:
-        users[user] = User(Day())
-    else:
-        users[user].user_day = Day()
+    day = Day()
+    state = TimetableMessageStateDto.TimetableMessageStateDto(
+        dialog_id=message.chat.id,
+        day=day.week_day,
+        week_type=day.week_type,
+        user_id=user,
+        timestamp=message.date.timestamp(),
+        message_id=message.message_id,
+        group='test'
+    )
 
-    await message.answer(text=format_timetable_text(users[user].user_day.week_type,
-                                                    users[user].user_day.week_day),
-                         reply_markup=days_scroll,
-                         parse_mode="HTML")
+    msg = await message.answer(text=format_timetable_text(state.week_type,
+                                                          state.day),
+                               reply_markup=days_scroll,
+                               parse_mode="HTML")
+    state.message_id = msg.message_id
+    client.add_state(state)
+    print(msg.message_id)
 
 
 def register_timetable(dp: Dispatcher):
@@ -29,22 +45,34 @@ def register_timetable(dp: Dispatcher):
 
 async def scroll_previous_day(call: CallbackQuery):
     await call.answer(cache_time=1)
-    user = call.from_user.id
 
     callback_data = call.data
 
+    state = client.get_state(dialog_id=call.message.chat.id, message_id=call.message.message_id)
+
+    if not state:
+        logging.warning(f"State not exist")
+        logging.info(f"{callback_data=}")
+        logging.info(f"{call.message}")
+        logging.info(f"{call.message.from_user.id}")
+        await call.message.edit_text(text='Ошибка, запросите новое расписание!',
+                                     parse_mode="HTML")
+        return
+
     logging.info(f"{callback_data=}")
 
-    if users[user].user_day.week_day == 1:
-        users[user].user_day.week_type = 2 if users[user].user_day.week_type == 1 else 1
-        users[user].user_day.week_day = 7
+    if state.day == 1:
+        state.week_type = 2 if state.week_type == 1 else 1
+        state.day = 7
     else:
-        users[user].user_day.week_day -= 1
+        state.day -= 1
 
-    await call.message.edit_text(text=format_timetable_text(users[user].user_day.week_type,
-                                                            users[user].user_day.week_day),
-                                 parse_mode="HTML")
+    edited_msg = await call.message.edit_text(text=format_timetable_text(state.week_type, state.day),
+                                              parse_mode="HTML")
     await call.message.edit_reply_markup(reply_markup=days_scroll)
+
+    state.timestamp = edited_msg.edit_date.timestamp()
+    client.add_state(state)
 
 
 def register_previous_day(dp: Dispatcher):
@@ -54,22 +82,34 @@ def register_previous_day(dp: Dispatcher):
 
 async def scroll_next_day(call: CallbackQuery):
     await call.answer(cache_time=1)
-    user = call.from_user.id
 
     callback_data = call.data
 
+    state = client.get_state(dialog_id=call.message.chat.id, message_id=call.message.message_id)
+
+    if not state:
+        logging.warning(f"State not exist")
+        logging.info(f"{callback_data=}")
+        logging.info(f"{call.message}")
+        logging.info(f"{call.message.from_user.id}")
+        await call.message.edit_text(text='Ошибка, запросите новое расписание!',
+                                     parse_mode="HTML")
+        return
+
     logging.info(f"{callback_data=}")
 
-    if users[user].user_day.week_day == 7:
-        users[user].user_day.week_type = 2 if users[user].user_day.week_type == 1 else 1
-        users[user].user_day.week_day = 1
+    if state.day == 7:
+        state.week_type = 2 if state.week_type == 1 else 1
+        state.day = 1
     else:
-        users[user].user_day.week_day += 1
+        state.day += 1
 
-    await call.message.edit_text(text=format_timetable_text(users[user].user_day.week_type,
-                                                            users[user].user_day.week_day),
-                                 parse_mode="HTML")
+    edited_msg = await call.message.edit_text(text=format_timetable_text(state.week_type, state.day),
+                                              parse_mode="HTML")
     await call.message.edit_reply_markup(reply_markup=days_scroll)
+
+    state.timestamp = edited_msg.edit_date.timestamp()
+    client.add_state(state)
 
 
 def register_next_day(dp: Dispatcher):
@@ -79,17 +119,29 @@ def register_next_day(dp: Dispatcher):
 
 async def change_week(call: CallbackQuery):
     await call.answer(cache_time=1)
-    user = call.from_user.id
 
     callback_data = call.data
 
+    state = client.get_state(dialog_id=call.message.chat.id, message_id=call.message.message_id)
+
+    if not state:
+        logging.warning(f"State not exist")
+        logging.info(f"{callback_data=}")
+        logging.info(f"{call.message}")
+        logging.info(f"{call.message.from_user.id}")
+        await call.message.edit_text(text='Ошибка, запросите новое расписание!',
+                                     parse_mode="HTML")
+        return
+
     logging.info(f"{callback_data=}")
 
-    users[user].user_day.week_type = 2 if users[user].user_day.week_type == 1 else 1
-    await call.message.edit_text(text=format_timetable_text(users[user].user_day.week_type,
-                                                            users[user].user_day.week_day),
-                                 parse_mode="HTML")
+    state.week_type = 2 if state.week_type == 1 else 1
+    edited_msg = await call.message.edit_text(text=format_timetable_text(state.week_type, state.day),
+                                              parse_mode="HTML")
     await call.message.edit_reply_markup(reply_markup=days_scroll)
+
+    state.timestamp = edited_msg.edit_date.timestamp()
+    client.add_state(state)
 
 
 def register_change_week(dp: Dispatcher):
